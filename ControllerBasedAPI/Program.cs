98a648b8ff32e15d493536de50f62d61b831364a
mcastre1 <miguelcastrejongal@gmail.com>
2026-08-31 09:38:09 -0600

    Controller based api, scaffolded todoitem controller http requests

---
+++ ControllerBasedAPI/Program.cs
@@ -0,0 +1,28 @@
+using ControllerBasedAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("TodoContext") ?? throw new InvalidOperationException("Connection string 'TodoContext' not found.");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
