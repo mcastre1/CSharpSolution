58319cf66309b102ad274bda208e75e2d6b34cc3
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-12 08:06:17 -0600

    Created db context

---
+++ SalesAPI/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesAPI.Models;
+
+namespace SalesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<SalesRep> SalesReps { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Sale> Sales { get; set; }
+        public DbSet<SaleItem> SaleItems { get; set; }
+
+
+    }
+}
