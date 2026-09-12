db47334ff5f9f24bcdc323dcd5bab554add24042
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-12 07:04:20 -0600

    Made all non nullable properties required

--- SalesAPI/Models/SaleItem.cs
+++ SalesAPI/Models/SaleItem.cs
@@ -9,7 +9,7 @@
         public decimal UnitPrice { get; set; }
         public decimal LineTotal => Quantity * UnitPrice;
 
-        public Sale Sale { get; set; }
-        public Product Product { get; set; }
+        public required Sale Sale { get; set; }
+        public required Product Product { get; set; }
     }
 }
