7ca1440e73f47ed1a66a03aa73ba4b9acc5d9379
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-13 08:31:40 -0600

    Created controller for Customer

---
+++ SalesAPI/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SalesAPI.Data;
+using SalesAPI.Models;
+
+namespace SalesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CustomersController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult> GetCustomers()
+        {
+            var customers = await _context.Customers.ToListAsync();
+            return Ok(customers);
+        }
+    }
+}
