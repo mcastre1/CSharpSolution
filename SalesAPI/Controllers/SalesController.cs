f887842b3e896a934a48ce76558e84c19915d891
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-14 09:07:57 -0600

    Finished sale controller.

--- SalesAPI/Controllers/SalesController.cs
+++ SalesAPI/Controllers/SalesController.cs
@@ -36,6 +36,7 @@ namespace SalesAPI.Controllers
 
         }
 
+        [HttpPost]
         public async Task<IActionResult> CreateSale(Sale sale)
         {
             var newSale = new Sale
@@ -44,6 +45,76 @@ namespace SalesAPI.Controllers
                 SalesRepId = sale.SalesRepId,
                 SaleDate = DateTime.UtcNow
             };
+
+            _context.Sales.Add(newSale);
+            await _context.SaveChangesAsync();
+
+            foreach (var item in sale.SaleItems)
+            {
+                _context.SaleItems.Add(new SaleItem
+                {
+                    SaleId = newSale.Id,
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    UnitPrice = item.UnitPrice
+                });
+            }
+
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetSale), new { id = newSale.Id }, newSale);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSale(int id, Sale sale)
+        {
+            if(id != sale.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingSale = await _context.Sales.Include(s => s.SaleItems).FirstOrDefaultAsync(s => s.Id == id);
+
+            if(existingSale == null)
+            {
+                return NotFound();
+            }
+
+            existingSale.CustomerId = sale.CustomerId;
+            existingSale.SalesRepId = sale.SalesRepId;
+
+            _context.SaleItems.RemoveRange(existingSale.SaleItems);
+
+            foreach (var item in sale.SaleItems)
+            {
+                _context.SaleItems.Add(new SaleItem
+                {
+                    SaleId = existingSale.Id,
+                    ProductId = item.ProductId,
+                    Quantity = item.Quantity,
+                    UnitPrice = item.UnitPrice
+                });
+            }
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSale(int id)
+        {
+            var existingSale = await _context.Sales.Include(s => s.SaleItems).FirstOrDefaultAsync(s => s.Id == id);
+
+            if (existingSale == null)
+            {
+                return NotFound();
+            }
+
+            _context.SaleItems.RemoveRange(existingSale.SaleItems);
+            _context.Sales.Remove(existingSale);
+
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
     }
 }
