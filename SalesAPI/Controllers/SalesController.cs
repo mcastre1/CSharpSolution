19f84c12a6e848764c428b2bad55a155c368b1ff
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-14 08:16:16 -0600

    Created get all sales including its sale items.

---
+++ SalesAPI/Controllers/SalesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using SalesAPI.Data;
+
+namespace SalesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public SalesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var sales = await _context.Sales.Include(s => s.SaleItems).ToListAsync();
+            return Ok(sales);
+        }
+    }
+}
