aebddddca26bc67c7060aa72361e5313a7c80b8c
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-16 21:13:40 -0600

    Created a dto for sales rep creation to prevent user from passing in id

---
+++ SalesAPI/Dtos/SalesRepCreateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SalesAPI.Dtos
+{
+    public class SalesRepCreateDto
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+    }
+}
