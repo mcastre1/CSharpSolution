1ee726d524da602924059192d815ec9a7e723a9d
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-20 21:45:43 -0600

    Created a dto for sale creation

---
+++ SalesAPI/Dtos/SaleCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using SalesAPI.Models;
+
+namespace SalesAPI.Dtos
+{
+    public class SaleCreateDto
+    {
+        public int CustomerId { get; set; }
+        public int SalesRepId { get; set; }
+        public DateTime SaleDate { get; set; }
+
+        public List<SaleItemDto> SaleItems { get; set; } = new();
+    }
+}
