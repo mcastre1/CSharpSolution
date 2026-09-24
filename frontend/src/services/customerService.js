a374e0186f28708a8cefe2ad2ee495103e8ca062
mcastre1 <miguelcastrejongal@gmail.com>
2026-09-23 21:34:38 -0600

    Only passing back response data instead of the whole json object when calling api from fronted.

--- frontend/src/services/customerService.js
+++ frontend/src/services/customerService.js
@@ -3,10 +3,11 @@ import axios from "axios";
 const API = "https://localhost:7152/api";
 
 export async function getAllCustomers() {
-    console.log("Hello");
-    return axios.get(`${API}/customers`);
+    var res = await axios.get(`${API}/customers`);
+    return res.data;
 }
 
 export async function getCustomer(id) {
-    return axios.get(`${API}/customers/${id}`)
+    var res = await axios.get(`${API}/customers/${id}`);
+    return res.data
 }
